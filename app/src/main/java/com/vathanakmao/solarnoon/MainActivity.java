ac832d9da140011ca88a0aa0c53db81a7341c8a9
Vathanak Mao <vathanakmao@gmail.com>
2024-02-23 17:35:52 +0700

    Used getCurrentLocation() instead of getLastLocation()

--- app/src/main/java/com/vathanakmao/solarnoon/MainActivity.java
+++ app/src/main/java/com/vathanakmao/solarnoon/MainActivity.java
@@ -1,14 +1,94 @@
 package com.vathanakmao.solarnoon;
 
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.location.CurrentLocationRequest;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.CancellationToken;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnTokenCanceledListener;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
+    public static final int MYPERMISSIONREQUESTCODE_GETCURRENTLOCATION = 1;
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION) != PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[] {ACCESS_COARSE_LOCATION}, MYPERMISSIONREQUESTCODE_GETCURRENTLOCATION);
+            Log.d(getLocalClassName(), "Permissions have been requested!");
+        } else {
+            Log.d(getLocalClassName(), "Permissions were already granted!");
+            calculateSolarNoonTime();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        Log.d(getLocalClassName(), "onRequestPermissionResult() called");
+
+        switch (requestCode) {
+            case MYPERMISSIONREQUESTCODE_GETCURRENTLOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PERMISSION_GRANTED) {
+                    Log.d(getLocalClassName(), "Permissions have been granted!");
+                    calculateSolarNoonTime();
+                } else {
+                    Log.d(getLocalClassName(), "Permissions have been denied!");
+                }
+                return;
+            }
+        }
+    }
+
+    private void calculateSolarNoonTime() {
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION) == PERMISSION_GRANTED) {
+            Task<Location> task = fusedLocationProviderClient.getCurrentLocation(new CurrentLocationRequest.Builder().build(), null);
+            task.addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    Log.d(getLocalClassName(), "onSuccess() called");
+
+                    if (location != null) {
+                        Log.d(getLocalClassName(), "Location: lat=" + location.getLatitude() + ", lon=" + location.getLongitude());
+                    }
+                }
+            });
+            task.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.d(getLocalClassName(), e.getStackTrace().toString());
+                }
+            });
+        }
     }
 }
